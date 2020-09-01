f8f720b2b5087ba1525f6e08d2f65851cc060c16
Bobsicle <Imadgical@gmail.com>
2020-08-31 19:10:21 -0600

    Day's lecture on character movement

---
+++ 2670 project/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 9.1f;
+    public float moveSpeed = 3f;
+    public float fastMoveSpeed;
+    public float jumpForce = 10f;
+    public int jumpCountMax;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = moveSpeed;
+        
+        if (Input.GetKey(KeyCode.Y))
+        {
+            movement.x *= -moveSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
+    }
+}
