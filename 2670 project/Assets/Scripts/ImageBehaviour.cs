ea939fe805ccaab30514f5c1c15816dff1962f5d
Bobsicle <Imadgical@gmail.com>
2020-09-08 22:41:34 -0600

    practice

---
+++ 2670 project/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageBehavior : MonoBehaviour
+{
+    private Image imageObj;
+
+    public FloatData dataObj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        imageObj.fillAmount = dataObj.value;
+    }
+}
