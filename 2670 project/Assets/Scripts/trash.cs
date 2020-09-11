994bd9551fba0820abee3e96000f30f7049f59dc
Bobsicle <Imadgical@gmail.com>
2020-09-10 23:36:54 -0600

    practice

---
+++ 2670 project/Assets/Scripts/trash.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    public int loopCountdown;
+    // Start is called before the first frame update
+    private IEnumerator Timer()
+    {
+        while (loopCountdown > 0)
+        {
+            print(loopCountdown);
+            loopCountdown--;
+            yield return new WaitForSeconds(1);
+        }
+    }
+    
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+}
