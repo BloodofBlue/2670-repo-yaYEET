71dd2f02c398b21c9933326e483cc071d5a588f3
Bobsicle <Imadgical@gmail.com>
2020-09-15 23:08:17 -0600

    Character controller take 2
    Iunno what i did, but i sure broke stuff so here we go again

---
+++ 2670 project/Assets/Scripts/NewCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class NewCharacterController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+
+    public float moveSpeed = 5f, rotateSpeed = 120f, gravity = -9.81f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var vInput = Input.GetAxis("Vertical")*moveSpeed;
+        movement.Set(vInput,gravity,0 );
+
+        var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,hInput,0);
+
+        movement = transform.TransformDirection(movement);
+        
+        controller.Move(movement * Time.deltaTime);
+    }
+}
