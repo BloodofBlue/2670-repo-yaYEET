ec38bdcf52cd0e0e5be6afc0bb6ec67ef8413924
Bobsicle <Imadgical@gmail.com>
2020-09-16 07:11:57 -0600

    added jump

--- 2670 project/Assets/Scripts/NewCharacterController.cs
+++ 2670 project/Assets/Scripts/NewCharacterController.cs
@@ -9,7 +9,11 @@ public class NewCharacterController : MonoBehaviour
     private CharacterController controller;
     private Vector3 movement;
 
-    public float moveSpeed = 5f, rotateSpeed = 120f, gravity = -9.81f;
+    public float moveSpeed = 5f, rotateSpeed = 120f, gravity = -9.81f, jumpForce = 30f;
+    private float yVar;
+    public int jumpCountMax = 2;
+    private int jumpCount;
+    
 
     private void Start()
     {
@@ -19,10 +23,24 @@ public class NewCharacterController : MonoBehaviour
     private void Update()
     {
         var vInput = Input.GetAxis("Vertical")*moveSpeed;
-        movement.Set(vInput,gravity,0 );
+        movement.Set(vInput,yVar,0 );
 
         var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
         transform.Rotate(0,hInput,0);
+
+        yVar += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yVar = -1f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            yVar = jumpForce;
+            jumpCount++;
+        }
 
         movement = transform.TransformDirection(movement);
         
