23d20a2a793da22dbaed17cd47aa4e2159d6f6cd
Bobsicle <Imadgical@gmail.com>
2020-09-18 23:09:48 -0600

    minor setup so i can hopefully catch up soon
    I had it aaaall planned out how i would catch up over the weekend, but NOOOOO the apartment has to start back up the goddang CLEANING CHECKS so now i have to spend my time on THAT instead of my CODE otherwise im gonna get FINED and im  MAD

---
+++ 2670 project/Assets/Scripts/Notes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // I hecking forgot what i wanted to write dow here, but i have this for future use now i guess?
+    // I am a forgetful fool and hopefully this will help me remember what my plans are regarding my code
+}
