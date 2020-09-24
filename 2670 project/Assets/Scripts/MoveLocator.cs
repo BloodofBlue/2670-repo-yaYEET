09e5008d89be686ad38c7bf1cd12182a3cbb45e0
Bobsicle <Imadgical@gmail.com>
2020-09-23 22:38:42 -0600

    lerp and lookat

---
+++ 2670 project/Assets/Scripts/MoveLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    private Camera cam;
+    public Transform pointobj;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        var location = cam.ScreenToWorldPoint(Input.mousePosition);
+        //var hitpoint =
+    }
+}
